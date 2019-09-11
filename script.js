e22c29ef9d7c6fc1406c69a6cb99a92fe644b8ab
Marina <stahmarinaoleg@gmail.com>
2019-09-11 11:52:51 +0300

    commit task 2

--- script.js
+++ script.js
@@ -10,10 +10,42 @@ let appData = {
 	savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
+// for(let i = 0; i < 2; i++){
+// let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//     b = prompt("Во сколько обойдется?", '');
+
+//     if((typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null &&
+//         a != "" && b != "" && a.length < 50){
+//             console.log("done");
+//             appData.expenses[a] = b;
+//         }   
+// }
+
+// let i = 0;
+// while (i < 2){
+//     let a = prompt("1", ''),
+//     b = prompt("2", '');
+
+//     if((typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null &&
+//         a != "" && b != "" && a.length < 50){
+//             console.log("done");
+//             appData.expenses[a] = b;
+//         }  
+//     i++;      
+// }
+
+let i = 0;
+do{
+    let a = prompt("1", ''),
+    b = prompt("2", '');
+
+    if((typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null &&
+        a != "" && b != "" && a.length < 50){
+            console.log("done");
+            appData.expenses[a] = b;
+        }  
+    i++;     
+}while(i < 2);
 
 appData.expenses.a1 = a2;
 appData.expenses.a3 = a4;
